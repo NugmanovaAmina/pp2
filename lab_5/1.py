01a9c25131db94bf99443b1da1ad61b0863eb702
NugmanovaAmina <157198697+NugmanovaAmina@users.noreply.github.com>
2024-03-01 22:35:06 +0600

    Right version 1.py

--- lab_5/1.py
+++ lab_5/1.py
@@ -1,7 +1,6 @@
 import re
 
 def match_string(pattern, text):
-    # Use re.match() to see if the pattern matches the beginning of the string
     match = re.match(pattern, text)
     
     if match:
@@ -9,13 +8,10 @@ def match_string(pattern, text):
     else:
         print("No match found")
 
-# Pattern to match 'a' followed by zero or more 'b's
 pattern = r'a*b*'
 
-# Test strings
 test_strings = ["ab", "abb", "aab", "a", "b", "bb", "abbb"]
 
-# Match each test string against the pattern
 for text in test_strings:
     print("Testing:", text)
     match_string(pattern, text)
